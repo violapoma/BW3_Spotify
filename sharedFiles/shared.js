5f0dce92713fcd4c7a4aa8d5d8a1388848a17d21
patricksgro <patricksgro02@gmail.com>
2025-07-02 10:17:53 +0200

    Creazione struttura condivisa

---
+++ sharedFiles/shared.js
@@ -0,0 +1,29 @@
+
+//FUNZIONAMENTO TENDINA AMICI (COLONNA DESTRA)
+document.addEventListener('DOMContentLoaded', () => {
+            const rightCol = document.getElementById('rightCol');
+            const middleCol = document.getElementById('middleCol');
+            const closeBtn = document.getElementById('closeRightCol');
+            const openBtn = document.getElementById('openRightCol');
+
+            function closeRightCol() {
+                rightCol.classList.add('d-none');
+                rightCol.classList.remove('col-lg-2');
+
+                middleCol.classList.replace('col-lg-7', 'col-lg-9');
+
+                openBtn.style.display = 'block';
+            }
+
+            function openRightCol() {
+                rightCol.classList.remove('d-none');
+                rightCol.classList.add('col-lg-2');
+
+                middleCol.classList.replace('col-lg-9', 'col-lg-7');
+
+                openBtn.style.display = 'none';
+            }
+
+            closeBtn.addEventListener('click', closeRightCol);
+            openBtn.addEventListener('click', openRightCol);
+        });
