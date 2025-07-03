6059b02c5d0c6168339a6fc67b155720ccf3602f
patricksgro <patricksgro02@gmail.com>
2025-07-03 20:42:01 +0200

    Salvataggio modifiche prima pull main

--- sharedFiles/shared.js
+++ sharedFiles/shared.js
@@ -1,29 +1,54 @@
 
 //FUNZIONAMENTO TENDINA AMICI (COLONNA DESTRA)
 document.addEventListener('DOMContentLoaded', () => {
-            const rightCol = document.getElementById('rightCol');
-            const middleCol = document.getElementById('middleCol');
-            const closeBtn = document.getElementById('closeRightCol');
-            const openBtn = document.getElementById('openRightCol');
+    const rightCol = document.getElementById('rightCol')
+    const middleCol = document.getElementById('middleCol')
+    const closeBtn = document.getElementById('closeRightCol')
+    const openBtn = document.getElementById('openRightCol')
 
-            function closeRightCol() {
-                rightCol.classList.add('d-none');
-                rightCol.classList.remove('col-lg-2');
+    function closeRightCol() {
+        rightCol.classList.add('d-none')
+        rightCol.classList.remove('col-lg-2', 'col-md-3')
 
-                middleCol.classList.replace('col-lg-7', 'col-lg-9');
+        middleCol.classList.replace('col-lg-7', 'col-lg-9')
 
-                openBtn.style.display = 'block';
-            }
+        openBtn.style.display = 'block'
+    }
 
-            function openRightCol() {
-                rightCol.classList.remove('d-none');
-                rightCol.classList.add('col-lg-2');
+    function openRightCol() {
+        rightCol.classList.remove('d-none')
 
-                middleCol.classList.replace('col-lg-9', 'col-lg-7');
+        if (window.matchMedia('(max-width: 1285px)').matches) {
+            rightCol.classList.remove('col-lg-2')
+            rightCol.classList.add('col-md-3')
+        } else {
+            rightCol.classList.remove('col-md-3')
+            rightCol.classList.add('col-lg-2')
+        }
 
-                openBtn.style.display = 'none';
-            }
+        middleCol.classList.replace('col-lg-9', 'col-lg-7')
 
-            closeBtn.addEventListener('click', closeRightCol);
-            openBtn.addEventListener('click', openRightCol);
-        });
+        openBtn.style.display = 'none'
+    }
+
+    closeBtn.addEventListener('click', closeRightCol)
+    openBtn.addEventListener('click', openRightCol)
+
+
+    //condizioni responsive
+
+    function adjustRightCol() {
+        if (window.matchMedia('(max-width: 1285px)').matches) {
+            rightCol.classList.add('col-md-3')
+            rightCol.classList.remove('col-lg-2')
+        } else {
+            rightCol.classList.add('col-lg-2')
+            rightCol.classList.remove('col-md-3')
+        }
+    }
+
+    adjustRightCol()
+
+
+    window.addEventListener('resize', adjustRightCol)
+});
